da6f01ad3a5d47ec887f1cc0283c017a1c4eb0d7
jessyfouace <jessy.fouace62@gmail.com>
2018-11-13 11:23:49 +0100

    Delete table for stocking all json info and when user sort the table, did ajax request (better for update)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-// Create table for stock all information of the json
-class createUsers {
-    constructor() {
-        this.users = [];
+// Create ajax request
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let myObject = JSON.parse(xhttp.responseText)
+        let valueInput;
+        document.getElementById('select').onchange = function () {
+            valueInput = this.value;
+            htmlManager.changeOrder(myObject, valueInput);
+        }
+        htmlManager.changeOrder(myObject, valueInput);
     }
 }
-
-// For create the table initialize a new object
-let newuser = new createUsers();
+xhttp.open("GET", "info.json", true);
+xhttp.send();
 
 let htmlManager = {
     // Take the parent id for create element
@@ -46,15 +52,13 @@ let htmlManager = {
         htmlManager.setInfo(myObject)
     },
     // Method for sort the table, call all change of the value of input in html
-    changeOrder: function (takeValue) {
-        // Create sort
-        newuser.users.sort(function (first, last) {
+    changeOrder: function (object, takeValue) {
+        object.sort(function (first, last) {
             // Check all value of input and sort him
-            if (takeValue === "id") {
+            if (takeValue === "id" || takeValue === undefined) {
                 return first.id > last.id;
             }
             if (takeValue === "name") {
-
                 return first.name > last.name;
             }
             if (takeValue === "age") {
@@ -64,16 +68,6 @@ let htmlManager = {
                 return first.from > last.from;
             }
         });
-        htmlManager.viewHTML(newuser.users);
+        htmlManager.viewHTML(object);
     }
-}
-
-// Create ajax request
-let xhttp = new XMLHttpRequest();
-xhttp.open("GET", "info.json", false);
-xhttp.send(null);
-if (xhttp.status == 200) {
-    let myObject = JSON.parse(xhttp.responseText)
-    newuser.users = myObject;
-    htmlManager.viewHTML(newuser.users)
 }
